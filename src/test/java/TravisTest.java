1aeeb504b07e67263ececbafe9a0a1e2b4438dce
Gabbendorf <gabriellamds@yahoo.it>
2017-10-19 16:03:58 +0100

    fake tests to try out travis

--- src/test/java/TravisTest.java
+++ src/test/java/TravisTest.java
@@ -1,4 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TravisTest {
 
+    @Test
+    public void passingTest() {
+        assertEquals(1 + 1, 2);
+    }
 
+    @Test
+    public void failingTest() {
+        assertEquals(1 + 1, 3);
+    }
 }
